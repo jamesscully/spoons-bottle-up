309e154816d0cece6de17d0de50fb9328e3ddcc3
James Scully <james.will.scully@gmail.com>
2019-06-09 04:10:19 +0100

    Buttons Exit and Settings now work

--- app/src/main/java/com/scullyapps/spoonsbottleup/MainActivity.java
+++ app/src/main/java/com/scullyapps/spoonsbottleup/MainActivity.java
@@ -4,16 +4,43 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.mmenu_button_settings) Button btnSettings;
+    @BindView(R.id.mmenu_button_exit)     Button btnExit;
+    @BindView(R.id.mmenu_button_start)    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ButterKnife.bind(this);
+
         ActionBar toolbar = getSupportActionBar();
         toolbar.hide();
+
+        setupButtons();
+
+
+    }
+
+    private void setupButtons() {
+        btnSettings.setOnClickListener(v -> {
+            Intent settings = new Intent();
+            settings.setClass(this, SettingsActivity.class);
+            startActivity(settings);
+        });
+
+        btnExit.setOnClickListener(v -> {
+            System.exit(0);
+        });
     }
 }
